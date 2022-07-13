55d36ed5481d183f030108d1a58a9f2c22bfd410
prajaktahadole <prajaktahadole@gmail.com>
2022-07-13 23:54:32 +0530

    binary

---
+++ Revision/Binary.js
@@ -0,0 +1,79 @@
+// Binary Equivalent -96:7:46
+// Description
+
+// Given an integer n, you need to find out its binary representation
+// Input
+// The first line of the input contains one integer t (1 ≤ t ≤ 10) — the number of test cases. Then t test cases follow.
+// The first line of each test case contains a single integer n (1 ≤ n ≤ 100000) — the number.
+
+
+// Output
+// For each test case, print the answer: The binary representation of the integer.
+// Sample Input 1 
+// 2
+// 15
+// 128
+// Sample Output 1
+// 1111
+// 10000000
+
+
+function BinaryEquivalent(n){
+
+    var binary = [];
+    while(n > 0){
+       rem = n%2;
+       binary.push(rem);
+        n = Math.floor(n/2);
+       
+        
+    }
+     //console.log(binary);
+     var i = 0; 
+     var j = binary.length-1;
+     
+     while(i< j){
+        var temp = binary[i];
+         binary[i] = binary[j];
+         binary[j] = temp;
+         
+         i++;
+         j--;
+     }
+     console.log(binary.join(""));
+     
+ }
+ 
+ 
+ 
+function runProgram(input){
+     input = input.split('\n');
+     
+     for(var i = 1; i <= +input[0]; i++)
+     {
+         var n = +input[i];
+         BinaryEquivalent(n);
+     }
+ }
+ if (process.env.USER === "") {
+   runProgram(``);
+ } else {
+   process.stdin.resume();
+   process.stdin.setEncoding("ascii");
+   let read = "";
+   process.stdin.on("data", function (input) {
+     read += input;
+   });
+   process.stdin.on("end", function () {
+     read = read.replace(/\n$/, "");
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+   });
+   process.on("SIGINT", function () {
+     read = read.replace(/\n$/, "");
+     runProgram(read);
+     process.exit(0);
+   });
+ }
+ 
+ 
---
+++ Revision/BubbleSort.js
@@ -0,0 +1,55 @@
+// Bubble Sort Problem -96:6:17
+// Description
+
+// You are given an array of N unsorted numbers. Your task is to write BUBBLE SORT such that numbers present in the array gets sorted.
+
+// USING ANY OTHER ALGORITHM OR USING IN BUILT SORT FUNCTION WOULD LEAD TO YOUR DISQUALIFICATION
+
+
+// Input
+// Input Format:
+
+// First line of input contains N
+
+// Second line of input contains N numbers
+
+// Constraints:
+
+// N < 500
+
+
+// Output
+// Output the numbers given after sorting it in increasnig order
+
+
+// Sample Input 1 
+
+// 5
+// 3 5 0 9 8
+// Sample Output 1
+
+// 0 3 5 8 9
+
+
+function solve(N,arr){
+
+    for (i = 0; i<N-1; i++)
+    {
+            for (j = 0; j<N-i-1; j++)
+            {
+                if (arr[j] > arr[j + 1]) 
+                {
+                    swap(arr, j, j+1);
+                }
+            }
+    }
+    console.log(arr.join(" "));
+}
+
+
+function swap(arr, j, i){
+var temp = arr[j];
+arr[j] = arr[i];
+arr[i] = temp;
+}
+
